a525b5c22221adc9612314a519c7529634d1473b
ets <etienne.rinckel@etu.univ-amu.fr>
2022-09-04 22:40:27 +0200

    build ov homepage

--- src/data/dataQuatrieme.js
+++
@@ -1,7 +0,0 @@
-export default
-	{"cours" : [
-	{"name":'cour 1 4e introduction techno',
-	"pdf":'/Ressources/Cours/4e/beamer_Etienne_Rinckel_Cours_Techno_1_4e.pdf'}],
-	 "TD":[{"name":'TD 1 4e Prise en main des outils',
-	"pdf":'/Ressources/TD/4e/TD_1_4e_Prise_en_main_des_outils__r_alisation_d_un_diagramme_.pdf'}]
-	}
